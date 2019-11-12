b3bf0a258192452d66e9e56614b0eff5dab62443
singhgur8 <gurjotssingh510@gmail.com>
2019-11-11 21:04:33 -0800

    update and delete partially remaining

--- datastore/index.js
+++ datastore/index.js
@@ -10,7 +10,7 @@ var items = {};
 exports.create = (text, callback) => {
   counter.getNextUniqueId((err, id) => {
     if (err) {
-      throw err;
+      callback(new Error('error'));
     } else {
       // items[id] = text; //items[counterstring] = text
 
@@ -18,40 +18,86 @@ exports.create = (text, callback) => {
       // console.log('filepath is: ', filepath, '__dirname is: ', __dirname);
       fs.writeFile(filepath, text, (err) => {
         if (err) {
-          throw err;
+          callback(new Error('error'));
         } else {
           callback(null, { id, text });
         }
-        // console.log('this file was succesfilly created');
       });
-
     }
-
   });
 };
 
 exports.readAll = (callback) => {
   fs.readdir(exports.dataDir, (err, files) => {
-    return files;
+    if (err) {
+      callback(new Error('error'));
+    } else {
+      var data = _.map(files, (text, id) => {
+        text = text.slice(0, 5);
+        // console.log(text);
+        return { id: '0000' + (id + 1), text: '0000' + (id + 1) };
+      });
+      callback(err, data);
+
+      // return files;
+
+    }
   });
 
 
 
 
-  var data = _.map(items, (text, id) => {
-    return { id, text };
-  });
-  callback(null, data);
+  // callback(null, data);
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+
+  var filepath = path.join(exports.dataDir, `${id}.txt`);
+  // fs.readdir(filepath, (err, files) => {
+  //   console.log(files);
+  //   if (err) {
+  //     callback(new Error('error'));
+  //   } else {
+  //     console.log(files);
+  //   }
+
+  fs.readFile(filepath, 'utf8', function (err, text) {
+    if (err) {
+      callback(new Error('error'));
+    } else {
+      callback(null, { id, text });
+      return { text };
+    }
+  });
+
+
+
+  // var text = items[id];
+  // if (!text) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  // }
 };
+
+// exports.update = (id, text, callback) => {
+//   //if file path throws err, we throw err
+//   //else on sucess case, do the below thing
+//   var filepath = path.join(exports.dataDir, `${id}.txt`);
+
+//   fs.access(filepath, err => {
+//     if (err) {
+//       callback(new Error('error'));
+//     } else {
+//       fs.writeFile(filepath, text, function (err) {
+//         if (err) {
+//           callback(new Error('error'));
+//         } else {
+//           callback(null, { id, text });
+//         }
+
+//       });
+//     }
+//   });
 
 exports.update = (id, text, callback) => {
   var item = items[id];
@@ -63,15 +109,39 @@ exports.update = (id, text, callback) => {
   }
 };
 
+
+
+
+
+// var item = items[id];
+// if (!item) {
+//   callback(new Error(`No item with id: ${id}`));
+// } else {
+//   items[id] = text;
+//   callback(null, { id, text });
+// }
+// };
+
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  var filepath = path.join(exports.dataDir, `${id}.txt`);
+  fs.unlink(filepath, (err) => {
+    if (err) {
+      callback(new Error('error'));
+    } else {
+      console.log('deleted');
+      callback(null);
+    }
+  });
+
+
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
